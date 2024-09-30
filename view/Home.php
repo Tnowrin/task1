39a39c092f5156e2d5797078a6b6384ea5c29e8c
Tnowrin <121345502+Tnowrin@users.noreply.github.com>
2024-10-01 04:11:10 +0600

    Add files via upload

---
+++ view/Home.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+   
+    <title>Banking Website</title>
+
+    <link rel="stylesheet" href="../style/mystyle.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet"> 
+    
+</head>
+
+<body>
+    <div class="logo">
+        <p class ="pogo">BankPro</p>
+    </div>
+  
+    <div class="sticky">
+        <table>
+            <tr>
+                <td><a href="#">Home</a></td>
+                    <td><a href="#">About Us</a></td>
+                    <td><a href="#">Services</a></td>
+                    <td><a href="#">Contact Us</a></td>
+                    <td><a href="#">Login</a></td>
+            </tr>
+    </table>
+    </div>
+  
+    
+<div class="relative">
+            <h1 class ="h">Welcome to BankPro</h1>
+            <p class ="text">Your trusted partner for financial solutions</p>
+            <button class="button_button1">Sign Up</button>
+            <button class="button_button2">About US</button>
+</div>
+          
+      
+
+
+    <div class="fixed">
+        <p>&copy; 2024 BankPro. All Rights Reserved.</p>
+    </div>
+
+</body>
+
+</html>
